040ffe430e48b62438b84234f1ca145f68380b2f
iangrunt <ian@gruntwork.io>
2022-08-08 15:28:22 -0400

    init

--- chapter-1/development/main.tf
+++ chapter-1/development/main.tf
@@ -1,26 +1,26 @@
 module "vpc" {
   source = "./modules/networking"
 
-  name = "production-vpc"
+  name = "development-vpc"
 
-  cidr        = "10.1.0.0/16"
-  subnet_size = "extra-large"
+  cidr        = "10.2.0.0/16"
+  subnet_size = "small"
 }
 
 module "aurora" {
   source = "../modules/data"
 
-  name = "production-databse"
+  name = "development-databse"
 
   vpc_id      = module.vpc.id
-  server_size = "extra-large"
+  server_size = "small"
 }
 
 module "eks-cluster" {
   source = "../modules/kubernetes"
 
-  name = "production-kubernetes"
+  name = "development-kubernetes"
 
   vpc_id       = module.vpc.id
-  cluster_size = "extra-large"
+  cluster_size = "small"
 }
--- chapter-1/modules/aurora/main.tf
+++
@@ -1,5 +0,0 @@
-resource "null_resource" "this" {
-  triggers = {
-    destroy_if_name_changed = var.name
-  }
-}
